f05445f940b04d581f3fd322081f038c0271f8c1
randomusert <127417963+randomusert@users.noreply.github.com>
2023-07-07 19:16:17 +0300

    command system in alpha state

---
+++ tinv1/command/command.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sys = Cosmos.System;
+
+namespace tinv1.command
+{
+    internal class command
+    {
+    }
+}
